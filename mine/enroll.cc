8afd1259cc43da61a16553defac2f1eab96a1e98
Lorenzo Tamer <lorenzino65@users.noreply.github.com>
2021-07-16 20:25:12 +0200

    Okay my first changes

---
+++ mine/enroll.cc
@@ -0,0 +1,50 @@
+// License: Apache 2.0. See LICENSE file in root directory.
+// Copyright(c) 2020-2021 Intel Corporation. All Rights Reserved.
+
+#include "RealSenseID/FaceAuthenticator.h"
+#include <iostream>
+#include <stdio.h>
+
+class MyEnrollClbk : public RealSenseID::EnrollmentCallback
+{
+public:
+    void OnResult(const RealSenseID::EnrollStatus status) override
+    {
+        std::cout << "Result " << status << std::endl;
+    }
+
+    void OnProgress(const RealSenseID::FacePose pose) override
+    {
+        std::cout << "OnProgress " << pose << std::endl;
+    }
+
+    void OnHint(const RealSenseID::EnrollStatus hint) override
+    {
+        std::cout << "Hint " << hint << std::endl;
+    }
+
+    void OnFaceDetected(const std::vector<RealSenseID::FaceRect>& faces, const unsigned int ts) override
+    {
+        for (auto& face : faces)
+        {
+            printf("** Detected face %u,%u %ux%u (timestamp %u)\n", face.x, face.y, face.w, face.h, ts);
+        }
+    }
+};
+
+int main()
+{
+    RealSenseID::FaceAuthenticator authenticator;
+#ifdef _WIN32
+    auto status = authenticator.Connect({"COM1"});
+#elif LINUX
+    auto status = authenticator.Connect({"/dev/ttyACM0"});
+#endif
+    if (status != RealSenseID::Status::Ok)
+    {
+        std::cout << "Failed connecting with status " << status << std::endl;
+        return 1;
+    }
+    MyEnrollClbk enroll_clbk;
+    authenticator.Enroll(enroll_clbk, "john");
+}
